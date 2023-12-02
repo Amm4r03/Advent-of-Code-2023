c1b64d141f12a45254d6a98efe94f07119e9fd40
ammar <akidwai03@gmail.com>
2023-12-02 22:07:36 +0530

    first commit

---
+++ day1/day1Code.py
@@ -0,0 +1,17 @@
+sum = 0
+
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+with open("C:\\Users\\ahmad\\Desktop\\Ammar\\College\\code\\Advent of Code 2023\\day1\\CalibrationDocument.txt", "r") as file:
+    for line in file:
+        digitSen = ''
+        for char in line:
+            if char in NUMBERS:
+                digitSen = digitSen + char
+        if (digitSen):
+            lineSum = lineSum = int(digitSen[0]) * 10 + int(digitSen[-1])
+            sum = lineSum + sum
+
+print(sum)
+
+# ans = 56108

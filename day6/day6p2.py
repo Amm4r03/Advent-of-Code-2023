eeff96b35ad2b5543e4f37a60448ade9a9242fad
ammar <akidwai03@gmail.com>
2023-12-16 22:53:40 +0530

    code for d6 - working

--- day6/day6p2.py
+++ day6/day6p2.py
@@ -1,1 +1,21 @@
 # code for day 6 part 2
+
+with open("day6\\input.txt", 'r') as file:
+    text = file.read().splitlines()
+
+times = text[0][13::]
+time = (int)("".join(times.split()))
+
+distances = text[1][11::].split()
+distance = (int)("".join(distances))
+
+recordBroken = 0
+
+for i in range(time):
+    distanceCovered = i * (time - i)
+    if distanceCovered > distance:
+        recordBroken += 1
+        
+print(recordBroken)
+
+# answer = 34123437
